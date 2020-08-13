3c1628ae7260163fda0e67eb08059af88d1be91f
yinmay <1102316727@qq.com>
2020-08-12 22:28:27 -0400

    hook

---
+++ hooks/usePost.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+type Post = {
+  id: string;
+  date: string;
+  title: string;
+};
+
+export const usePost = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isEmpty, setIsEmpty] = useState(false);
+  useEffect(() => {
+    setIsLoading(true);
+    axios.get('/api/posts').then(
+      (res) => {
+        setPosts(res.data);
+        if (res.data.length === 0) {
+          setIsEmpty(true);
+        }
+        setIsLoading(false);
+      },
+      () => setIsLoading(false)
+    );
+  }, []);
+  return { isLoading, isEmpty, posts, setPosts, setIsLoading, setIsEmpty };
+};
